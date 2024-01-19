285a4890aeb35a0a6e0a2664892b9199262fa8ba
Andrew Obler <andrewobler@gmail.com>
2024-01-18 23:30:32 -0700

    Clean up import style

--- src/audio.rs
+++ src/audio.rs
@@ -1,16 +1,21 @@
-use std::{cmp::min, sync::mpsc};
+use std::{
+    cmp::min,
+    sync::mpsc::{Receiver, Sender, TryRecvError},
+};
 
 use cpal::{
     self,
     traits::{DeviceTrait, HostTrait},
+    Device, SampleFormat, SizedSample, Stream, StreamConfig,
 };
+use dasp_sample::{FromSample, Sample, ToSample};
 use log::{error, warn};
 
 use crate::errors::CreateAudioStreamError;
 
 pub fn create_default_input_stream(
-    sink: mpsc::Sender<Vec<u8>>,
-) -> Result<cpal::Stream, CreateAudioStreamError> {
+    sink: Sender<Vec<u8>>,
+) -> Result<Stream, CreateAudioStreamError> {
     let host = cpal::default_host();
 
     let device = host
@@ -28,15 +33,15 @@ pub fn create_default_input_stream(
     let config = supported_config.with_max_sample_rate().into();
 
     match sample_format {
-        cpal::SampleFormat::U8 => build_input_stream::<u8>(sink, &device, &config),
-        cpal::SampleFormat::F32 => build_input_stream::<f32>(sink, &device, &config),
+        SampleFormat::U8 => build_input_stream::<u8>(sink, &device, &config),
+        SampleFormat::F32 => build_input_stream::<f32>(sink, &device, &config),
         _ => panic!("Unsupported input sample format: {sample_format}"),
     }
 }
 
 pub fn create_default_output_stream(
-    source: mpsc::Receiver<Vec<u8>>,
-) -> Result<cpal::Stream, CreateAudioStreamError> {
+    source: Receiver<Vec<u8>>,
+) -> Result<Stream, CreateAudioStreamError> {
     let host = cpal::default_host();
 
     let device = host
@@ -53,17 +58,17 @@ pub fn create_default_output_stream(
     let config = supported_config.with_max_sample_rate().into();
 
     match sample_format {
-        cpal::SampleFormat::U8 => build_output_stream::<u8>(source, &device, &config),
-        cpal::SampleFormat::F32 => build_output_stream::<f32>(source, &device, &config),
+        SampleFormat::U8 => build_output_stream::<u8>(source, &device, &config),
+        SampleFormat::F32 => build_output_stream::<f32>(source, &device, &config),
         _ => panic!("Unsupported output sample format: {sample_format}"),
     }
 }
 
-fn build_input_stream<T: cpal::SizedSample + dasp_sample::ToSample<u8>>(
-    sink: mpsc::Sender<Vec<u8>>,
-    device: &cpal::Device,
-    config: &cpal::StreamConfig,
-) -> Result<cpal::Stream, CreateAudioStreamError> {
+fn build_input_stream<T: SizedSample + ToSample<u8>>(
+    sink: Sender<Vec<u8>>,
+    device: &Device,
+    config: &StreamConfig,
+) -> Result<Stream, CreateAudioStreamError> {
     let data_fn = move |data: &[T], _: &_| {
         if data.is_empty() {
             warn!("CPAL provided empty input slice");
@@ -82,11 +87,11 @@ fn build_input_stream<T: cpal::SizedSample + dasp_sample::ToSample<u8>>(
     Ok(device.build_input_stream(config, data_fn, err_fn, None)?)
 }
 
-fn build_output_stream<T: cpal::SizedSample + dasp_sample::FromSample<u8>>(
-    source: mpsc::Receiver<Vec<u8>>,
-    device: &cpal::Device,
-    config: &cpal::StreamConfig,
-) -> Result<cpal::Stream, CreateAudioStreamError> {
+fn build_output_stream<T: SizedSample + FromSample<u8>>(
+    source: Receiver<Vec<u8>>,
+    device: &Device,
+    config: &StreamConfig,
+) -> Result<Stream, CreateAudioStreamError> {
     let data_fn = move |data: &mut [T], _: &_| {
         if data.is_empty() {
             warn!("CPAL provided empty output slice");
@@ -103,11 +108,11 @@ fn build_output_stream<T: cpal::SizedSample + dasp_sample::FromSample<u8>>(
 
                 write_silence(rest);
             }
-            Err(mpsc::TryRecvError::Empty) => {
+            Err(TryRecvError::Empty) => {
                 warn!("No available input in source");
                 write_silence(data);
             }
-            Err(mpsc::TryRecvError::Disconnected) => error!("Input source disconnected"),
+            Err(TryRecvError::Disconnected) => error!("Input source disconnected"),
         };
     };
 
@@ -116,8 +121,8 @@ fn build_output_stream<T: cpal::SizedSample + dasp_sample::FromSample<u8>>(
     Ok(device.build_output_stream(config, data_fn, err_fn, None)?)
 }
 
-fn write_silence<T: cpal::SizedSample>(data: &mut [T]) {
+fn write_silence<T: SizedSample>(data: &mut [T]) {
     for sample in data.iter_mut() {
-        *sample = cpal::Sample::EQUILIBRIUM;
+        *sample = Sample::EQUILIBRIUM;
     }
 }
